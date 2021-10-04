1b0104fd171636c0a3bf98864d95a8cf48f8574c
GustBitencourt <gubiten@gmail.com>
2021-10-04 17:41:53 -0300

    comandos e atravessar parede

--- javascript/script.js
+++ javascript/script.js
@@ -9,7 +9,7 @@ snake[0] = {
     y: 8 * box,
 }
 
-let direction = "right";
+let direction = "right"; 
 
 //função que cria o background
 function criarBG() {
@@ -25,7 +25,23 @@ function criarSnake() {
     }
 }
 
+document.addEventListener('keydown', update); //evento que pega a tecla
+
+function update(event) { //ifs com as tecla e validações de direções
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+
 function iniciarJogo() {
+    //ifs para fazer a cobra atravessar as "paredes"
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].x = 16 * box;
+
     criarBG();
     criarSnake();
 
@@ -46,7 +62,7 @@ function iniciarJogo() {
         x: snakeX,
         y: snakeY
     }
-    //adiciona elemento a frente (no array)
+    //adiciona elemento a frente (no array) movimento
     snake.unshift(newHead);
 }
 
